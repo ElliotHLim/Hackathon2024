c51e2a130e2f0609a57a6dd8cba357e308d3fc07
Phoebe Jeske <phoebejeske@phoebes-air.student.wheaton.edu>
2024-11-09 11:11:48 -0600

    removed some duplicate import statements

--- App.js
+++ App.js
@@ -4,8 +4,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './pages/HomeScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 import Login from './app/screens/Login';
 
 const Stack = createNativeStackNavigator();
